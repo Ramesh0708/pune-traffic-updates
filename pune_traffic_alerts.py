84c4b96b1e66d1185ea624d9197a61b063c6aaec
Ramesh Choudhary <61725029+Ramesh0708@users.noreply.github.com>
2026-06-12 04:54:23 +0530

    Change return value to true in traffic alert function
    Fix return value to always return true.

--- pune_traffic_alerts.py
+++ pune_traffic_alerts.py
@@ -52,7 +52,7 @@ def is_regular_run_time():
 
     print("Current IST:", now_ist.strftime("%Y-%m-%d %H:%M:%S"))
 
-    return now_ist.hour in [8, 12, 16]
+    return true
 
 # ---------- 50-Piece Pune Fun Facts / Trivia ----------
 PUNE_FACTS = [
